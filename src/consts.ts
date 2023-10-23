f976e48b0d50e39a7bce5c0f5d0fd3006b7196c7
Mutsuki <yukkeca@outlook.jp>
2023-10-23 21:59:13 +0900

    軽微な変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mutsuki Sota Portfolio.';
+export const SITE_TITLE = 'Mutsuki Portfolio.';
 export const SITE_DESCRIPTION = 'Developer in Japan.';
