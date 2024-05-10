1a428f63e864aee22862882822a96ced76f15a5d
Mutsuki <yukkeca@outlook.jp>
2024-05-10 18:16:17 +0900

    cliの操作が足りてなかったのでもう一度

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import db from "@astrojs/db";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   output: 'server',
-  site: 'https://example.com',
+  site: 'https://yukkevv.dev',
   integrations: [mdx(), sitemap(), db()]
 });
